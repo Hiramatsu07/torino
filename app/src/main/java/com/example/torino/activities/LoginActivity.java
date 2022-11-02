50f7138018a253933216a10a92a54338a1e32a89
Hiram Verdefronte <hiram.verdefronte@upa.edu.py>
2022-11-02 09:35:14 -0300

    Logo funcional y nuevas strings

--- app/src/main/java/com/example/torino/activities/LoginActivity.java
+++ app/src/main/java/com/example/torino/activities/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.torino.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,9 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate (Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    getSupportActionBar().hide();
     setContentView(R.layout.activity_inicio);
     }
     public void lanzarCrearCuenta(View view){
