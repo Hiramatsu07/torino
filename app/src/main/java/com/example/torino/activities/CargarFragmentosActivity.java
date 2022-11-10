6014081437b632503525bafc7d752e4e5a0fb593
Hiram Verdefronte <hiram.verdefronte@upa.edu.py>
2022-11-10 19:06:00 -0300

    Notificaciones ya funcionales

--- app/src/main/java/com/example/torino/activities/CargarFragmentosActivity.java
+++ app/src/main/java/com/example/torino/activities/CargarFragmentosActivity.java
@@ -1,9 +1,13 @@
 package com.example.torino.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
--- app/src/main/java/com/example/torino/fragments/FirstFragment.java
+++ app/src/main/java/com/example/torino/fragments/FirstFragment.java
@@ -2,9 +2,11 @@ package com.example.torino.fragments;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -116,5 +118,4 @@ public class FirstFragment extends Fragment {
             }
         });
     }
-
 }
